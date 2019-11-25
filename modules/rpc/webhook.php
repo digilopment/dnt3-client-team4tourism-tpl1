a6cbe7a3eff01849190b5739a30e7468b4846009
Tomas Doubek <thomas.doubek@gmail.com>
2019-11-25 15:03:20 +0100

    no extends class in RPC

--- modules/rpc/webhook.php
+++ modules/rpc/webhook.php
@@ -1,16 +1,17 @@
 <?php
-class rpcModulController extends rpcAbstractModulController{
-	
-	public function run(){
-		$rest 		= new Rest;
-		$db 		= new Db;
-		if($rest->webhook(2) == "json" && $rest->webhook(3) == "competition-register" && $rest->webhook(4)){ //o jeden vyssi webhook ako maximalnz mozny
-			include "register.php";
-		}else{
-			$rest->loadDefault();
-		}
-	}
-	
+
+class rpcModulController {
+
+    public function run() {
+        $rest = new Rest;
+        $db = new Db;
+        if ($rest->webhook(2) == "json" && $rest->webhook(3) == "competition-register" && $rest->webhook(4)) { //o jeden vyssi webhook ako maximalnz mozny
+            include "register.php";
+        } else {
+            $rest->loadDefault();
+        }
+    }
+
 }
 
 rpcModulController::run();
